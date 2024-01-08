4057e0aec46f8834876de24c8ef4610dc5b62afd
Nagacash <chosenfewrecords@hotmail.de>
2024-01-08 19:45:05 +0100

    new logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,8 +12,8 @@ function Hero() {
           className="object-contain"
         />
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Explore The <span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+          Explore your favorite  Anime Characters <span className="red-gradient">Designed by NAGA APPAREL </span>  
+          2024
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
